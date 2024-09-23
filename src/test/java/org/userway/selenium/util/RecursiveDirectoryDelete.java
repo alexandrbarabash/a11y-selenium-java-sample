4157272dad98994cbddf194bcb1f918a25032e57
Maksym Panov <maksym.panov@userway.org>
2024-09-23 10:14:59 +0300

    CA-3612 Added Manual test case

---
+++ src/test/java/org/userway/selenium/util/RecursiveDirectoryDelete.java
@@ -0,0 +1,31 @@
+package org.userway.selenium.util;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class RecursiveDirectoryDelete {
+
+    public static void deleteDirectoryRecursively(String pathStr) throws IOException {
+        var path = Path.of(pathStr);
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    private RecursiveDirectoryDelete() {}
+
+}
