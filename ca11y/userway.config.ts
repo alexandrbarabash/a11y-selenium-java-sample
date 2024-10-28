67aed92173900ad6f364c9cd64ac39f77a890fb3
Maksym Panov <maksym.panov@userway.org>
2024-10-28 19:29:18 +0200

    Generalized userway.config.ts

--- ca11y/userway.config.ts
+++ ca11y/userway.config.ts
@@ -2,10 +2,10 @@ import { Config } from '@userway/cicd-core';
 
 export default {
     token: process.env.USERWAY_TOKEN,
-    organization: 'volodymyr-kulyk-userway-org-igpgx',
-    project: 'a11y-selenium-java-sample',
+    organization: '<ORGANIZATION_SLUG>',
+    project: '<PROJECT_SLUG>',
     reportPaths: ['./uw-a11y-reports'],
-    server: "https://api.qa.userway.dev",
+    server: "https://api.userway.org",
     override: {
         'main': {
             retention: 'long',
